c696ddec1045ecc0023ebdc930c028e24a401c4d
muhammadilyas <muhammadilyas@intensivate.com>
2024-01-11 23:58:58 +0500

    changes

--- src/controllers/feeAccountController.ts
+++ src/controllers/feeAccountController.ts
@@ -46,9 +46,9 @@ export const feeAccountController = {
       // Constructing the filter object based on the provided request data
       const filter: any = {};
 
-      if (grade) {
-        filter.grade = grade;
-      }
+      // if (grade) {
+      //   filter.grade = grade;
+      // }
 
       if (status === "paid") {
         filter["feeAccount.paidAmount"] = { $ne: "0" };
@@ -56,46 +56,55 @@ export const feeAccountController = {
         filter["feeAccount.paidAmount"] = "0";
       }
 
-      if (month) {
-        const monthNumber = parseInt(month, 10);
+      // if (month) {
+      //   const monthNumber = parseInt(month, 10);
+      //   filter["feeAccount.dueDate"] = {
+      //     $expr: { $eq: [{ $month: "$feeAccount.dueDate" }, monthNumber] },
+      //   };
+      // }
+      if (month !== undefined) {
         filter["feeAccount.dueDate"] = {
-          $expr: { $eq: [{ $month: "$feeAccount.dueDate" }, monthNumber] },
+          $gte: new Date(`2024-${month}-01`),
+          $lt: new Date(`2024-${month}-31`),
         };
+      }
+      if (grade !== undefined) {
+        filter["grade"] = grade;
       }
 
       // Fetching students based on the constructed filter
       const students = await userService.find(filter);
-      const studentsWithTotals = students.map((student: any) => {
-        const totalPaidAmount = student.feeAccount.reduce(
-          (acc, curr) => acc + Number(curr.paidAmount),
-          0
-        );
-        const totalPayableAmount = student.feeAccount.reduce(
-          (acc, curr) => acc + Number(curr.payableAmount) - Number(curr.discount),
-          0
-        );
+      // const studentsWithTotals = students.map((student: any) => {
+      //   const totalPaidAmount = student.feeAccount.reduce(
+      //     (acc, curr) => acc + Number(curr.paidAmount),
+      //     0
+      //   );
+      //   const totalPayableAmount = student.feeAccount.reduce(
+      //     (acc, curr) => acc + Number(curr.payableAmount) - Number(curr.discount),
+      //     0
+      //   );
 
-        return {
-          _id: student._id,
-          firstName: student.firstName,
-          lastName: student.lastName,
-          email: student.email,
-          gender: student.gender,
-          grade: student.grade,
-          studentId: student.studentId,
-          verified: student.verified,
-          createdAt: student.createdAt,
-          updatedAt: student.updatedAt,
-          feeAccount: student.feeAccount,
-          totalPaidAmount,
-          totalPayableAmount,
-        };
-      });
+      //   return {
+      //     _id: student._id,
+      //     firstName: student.firstName,
+      //     lastName: student.lastName,
+      //     email: student.email,
+      //     gender: student.gender,
+      //     grade: student.grade,
+      //     studentId: student.studentId,
+      //     verified: student.verified,
+      //     createdAt: student.createdAt,
+      //     updatedAt: student.updatedAt,
+      //     feeAccount: student.feeAccount,
+      //     totalPaidAmount,
+      //     totalPayableAmount,
+      //   };
+      // });
 
-      console.log("studentsWithTotals", studentsWithTotals);
+      // console.log("studentsWithTotals", studentsWithTotals);
 
       return res.status(StatusCodes.OK).json({
-        data: { students: studentsWithTotals },
+        data: { students: students },
         message: ReasonPhrases.OK,
         status: StatusCodes.OK,
       });
